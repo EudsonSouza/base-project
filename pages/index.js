86763d7c7b62c4249b6edc82da71ee3d4420856e
Eudson Souza <eudson.souza@outlook.com>
2024-08-22 00:28:01 -0300

    Change home page message

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
+import React from "react";
+
 const Home = () => {
-  return <h1> Em Construção </h1>;
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-4xl font-bold text-center">
+        🚧 Art Gallery Under Construction 🚧
+      </h1>
+    </div>
+  );
 };
 
 export default Home;
