4024f6f560d1abbd3ae72bd2f36dea0598c21eb2
Eudson Souza <eudson.souza@outlook.com>
2024-08-21 23:23:03 -0300

    Modify home messa to "Em Construção"

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
-const Home = () =>{
-    return <h1>Catarina amo você, se você me ama da uma risadinha </h1>
-}
+const Home = () => {
+  return <h1> Catarina amo você, se você me ama da uma risadinha </h1>;
+};
+
+const Home2 = () => {
+  return <h1> Catarina amo você, se você me ama da uma risadinha </h1>;
+};
 
 export default Home;
